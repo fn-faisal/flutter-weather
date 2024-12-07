093334c7a7fe2464dac1720a6f80db04c3e0c14c
fn-faisal <faisal.fn154@gmail.com>
2024-12-08 00:03:15 +0500

    Search bar

--- lib/presentation/molecules/weather_search_bar.dart
+++ lib/presentation/molecules/weather_search_bar.dart
@@ -1,30 +1,94 @@
+import 'dart:async';
+import 'package:debounce_throttle/debounce_throttle.dart';
 import 'package:flutter/material.dart';
-import 'package:weather_app/presentation/molecules/clickable_asset.dart';
-import 'package:weather_app/utils/sizes.dart';
 
-class WeatherSearchBar extends StatelessWidget {
+List<String> _suggestions = [
+  "Sunny",
+  "Cloudy",
+  "Rainy",
+  "Thunderstorms",
+  "Snow",
+  "Foggy",
+  "Windy",
+  "Overcast",
+  "Partly Cloudy",
+  "Freezing Rain",
+];
+
+class WeatherSearchBar extends StatefulWidget {
 
   final String placeholder;
 
   const WeatherSearchBar({super.key, required this.placeholder});
 
   @override
+  State<WeatherSearchBar> createState() => _WeatherSearchBarState();
+}
+
+class _WeatherSearchBarState extends State<WeatherSearchBar> {
+  final searchController = SearchController();
+  List<String> suggestions = [];
+  bool isLoading = false;
+
+  Completer<List<String>> _completer = Completer();
+
+  late final Debouncer _debouncer = Debouncer(const Duration(milliseconds: 300),
+    initialValue: '',
+    onChanged: (value) {
+      _completer.complete(_fetchSuggestions(value)); // call the API endpoint
+    }
+  );
+
+  Future<List<String>> _fetchSuggestions(String query) async {
+    return _suggestions.where((s) => s.toLowerCase().startsWith(query.toLowerCase())).toList();
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return SearchBar(
-      padding: const WidgetStatePropertyAll(
-        EdgeInsets.only( left: Sizes.md, right: Sizes.lg )),
-      backgroundColor: WidgetStatePropertyAll(Colors.grey[50]),
-      elevation: const WidgetStatePropertyAll(0.0),
-      hintText: placeholder,
-      hintStyle: const WidgetStatePropertyAll(
-      TextStyle(
-        color: Color(0xFFC4C4C4)
-        )
-      ),
-      trailing: [ClickableAsset(
-        onTap: () => {}, 
-        assetPath: 'assets/icons/search.svg'
-      )],
+    return SearchAnchor(
+      searchController: searchController,  
+      builder: (context, controller) => 
+        SearchBar(
+          controller: controller,
+          onTap: () => searchController.openView(),
+          onChanged: (_) => searchController.openView(),
+        ),
+      suggestionsBuilder: (context, controller) async { 
+        _debouncer.value = controller.text;
+        _completer = new Completer();
+        return [FutureBuilder(
+          future: _completer.future, 
+          builder: (context, snapshot){ 
+            final data = snapshot.data;
+            if ( isLoading )
+              return CircularProgressIndicator();
+            
+            if ( data == null )
+              return Text('No results');
+            return ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: data.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: ListTile(
+                    title: Text(data[index]),
+                    onTap: () {
+                      searchController.closeView(data[index]);
+                    },
+                  ),
+                );
+              },
+            );
+          }
+        )];
+      },
     );
   }
 }
--- lib/presentation/templates/home_page_template.dart
+++ lib/presentation/templates/home_page_template.dart
@@ -29,6 +29,6 @@ class HomePageTemplate extends StatelessWidget {
           
         )
       ],
-          );
+    );
   }
 }
