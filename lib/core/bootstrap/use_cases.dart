80c15eaf315e0ffc9f1b0a62a58ac7ecd830e1fd
fn-faisal <faisal.fn154@gmail.com>
2024-12-13 12:32:55 +0500

    rename timezone

--- lib/core/bootstrap/use_cases.dart
+++ lib/core/bootstrap/use_cases.dart
@@ -3,7 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:weather_app/data/repositories/temperature_http_repository.dart';
 import 'package:weather_app/data/repositories/timezone_http_repository.dart';
 import 'package:weather_app/domain/use_cases/current_temperature_from_timezone_use_case.dart';
-import 'package:weather_app/domain/use_cases/find_timezon_by_name_use_case.dart';
+import 'package:weather_app/domain/use_cases/find_timezone_by_name_use_case.dart';
 
 List<Provider<dynamic>> getUseCases() =>
   [
@@ -12,8 +12,8 @@ List<Provider<dynamic>> getUseCases() =>
         temperatureRepository: TemperatureHttpRepository()
       ) 
     ),
-    Provider<FindTimezonByNameUseCase>
-      .value(value: FindTimezonByNameUseCase(
+    Provider<FindTimezoneByNameUseCase>
+      .value(value: FindTimezoneByNameUseCase(
         timezoneRepository: TimezoneHttpRepository()
       )
     ),
--- lib/domain/use_cases/find_timezone_by_name_use_case.dart
+++ lib/domain/use_cases/find_timezone_by_name_use_case.dart
@@ -3,10 +3,10 @@ import 'package:weather_app/domain/entities/timezone.dart';
 import 'package:weather_app/domain/repositories/i_timezone_repository.dart';
 import 'package:weather_app/domain/use_cases/i_use_case.dart';
 
-class FindTimezonByNameUseCase implements IUseCase<List<Timezone>, String> { 
+class FindTimezoneByNameUseCase implements IUseCase<List<Timezone>, String> { 
   final ITimezoneRepository timezoneRepository;
 
-  FindTimezonByNameUseCase({
+  FindTimezoneByNameUseCase({
     required this.timezoneRepository,
   });
 
